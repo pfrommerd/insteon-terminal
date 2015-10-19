b3aa979a6d8a365d07a5bdf4d421d73e0a22d618
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2015-10-19 08:07:27 -0400

    implemented getStatus() for motion sensor. it's hit and miss because the motion sensor is at sleep most of the time

--- python/motionsensor.py
+++ python/motionsensor.py
@@ -7,7 +7,7 @@ import message
 from device import Device
 from querier import Querier
 from querier import MsgHandler
-from dbbuilder import LightDBBuilder
+from dbbuilder import GenericDBBuilder
 from linkdb import LightDBRecordFormatter
 from us.pfrommer.insteon.cmd.msg import InsteonAddress
 
@@ -24,24 +24,31 @@ class StatusMsgHandler(MsgHandler):
 	def __init__(self, l):
 		self.label = l
 	def processMsg(self, msg):
-		tmp = msg.getByte("command2") & 0xFF
+		if msg.isExtended():
+			light = msg.getByte("userData11") & 0xFF
+			battery = msg.getByte("userData12") & 0xFF
+			iofun.out(self.label + " light level: " + format(light, 'd') +
+					  " battery level: " + format(battery, 'd')) 
+			return 1
+		else:
+			iofun.out(self.label + " unexpected direct message: " + msg.toString())
+		return 0
 		iofun.out(self.label + " = " + format(tmp, '02d'))
 		return 1
 
 class MotionSensor(Device):
+	"""==============  Insteon Motion Sensor ===============
+	   NOTE: 1) none of the database management commands works!
+	         2) only getStatus() has been tested, and even that one is hit and miss
+	         3) use modem.startWatch() / modem.stopWatch() to see incoming messages"""
 	def __init__(self, name, addr):
 		Device.__init__(self, name, addr)
-		self.dbbuilder = LightDBBuilder(addr, self.db)
+		self.dbbuilder = GenericDBBuilder(addr, self.db)
 		self.db.setRecordFormatter(LightDBRecordFormatter())
 
-	def ping(self):
-		"""ping()
-		pings device"""
-		self.querier.setMsgHandler(DefaultMsgHandler("ping"))
-		self.querier.querysd(0x0F, 0x01);
- 
 	def getStatus(self):
 		"""getStatus()
-		get current light level of device"""
-		self.querier.setMsgHandler(StatusMsgHandler("light level"))
-		self.querier.querysd(0x19, 0x0)
+		YOU MUST PRESS THE SENSOR BUTTON SHORTLY BEFORE SO IT'S ALIVE. TRY SEVERAL TIMES!"""
+		self.querier.setMsgHandler(StatusMsgHandler("status"))
+		return self.querier.queryext(0x2e, 0x0, [0,0,0])
+
