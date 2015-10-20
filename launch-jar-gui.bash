e3cb9386be944bae865f724ce905c427cbbb2c7b
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2015-10-19 20:39:16 -0400

    added insteon-terminal command, removed launch*.bash

--- launch-jar-gui.bash
+++
@@ -1,5 +0,0 @@
-echo "Note: the jars must first be built using ant jar before the application can be launched"
-echo "Also, for using PLMs, rxtx natives must first be installed on this system"
-
-
-java -cp "build/jars/insteon-terminal-gui.jar:lib/*:lib/hub/*:lib/logger/*" us.pfrommer.insteon.cmd.gui.GUI
