84651e569a26f40bccb0fde7a9f98aba25ff7b86
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2016-03-20 20:05:23 -0400

    added fanlinc device, but only implemented ping() and enterLinkingMode() yet.

---
+++ python/fanlinc.py
@@ -0,0 +1,58 @@
+#-------------------------------------------------------------------------------
+#
+# Insteon fanlinc
+#
+
+import iofun
+import message
+
+from device import Device
+from switch import Switch
+from querier import Querier
+from querier import MsgHandler
+from threading import Timer
+from linkdb import *
+from device import LinkRecordAdder
+from dbbuilder import GenericDBBuilder
+from linkdb import LightDBRecordFormatter
+
+from us.pfrommer.insteon.msg import Msg
+from us.pfrommer.insteon.msg import MsgListener
+from us.pfrommer.insteon.msg import InsteonAddress
+
+def out(msg = ""):
+	iofun.out(msg)
+def outchars(msg = ""):
+	iofun.outchars(msg)
+
+class DefaultMsgHandler(MsgHandler):
+	label = None
+	def __init__(self, l):
+		self.label = l
+	def processMsg(self, msg):
+		iofun.out(self.label + " got msg: " + msg.toString())
+		return 1
+
+class FanLinc(Device):
+	"""==============  Insteon FanLinc ==============="""
+	def __init__(self, name, addr):
+		Device.__init__(self, name, addr)
+		self.dbbuilder = GenericDBBuilder(addr, self.db)
+		self.db.setRecordFormatter(LightDBRecordFormatter())
+
+	def ping(self):
+		"""ping()
+		pings device"""
+		self.querier.setMsgHandler(DefaultMsgHandler("ping"))
+		self.querier.querysd(0x0F, 0x01);
+
+	def enterLinkingMode(self):
+		"""enterLinkingMode()
+		causes the device to enter linking mode"""
+		self.querier.setMsgHandler(DefaultMsgHandler("linking mode"))
+		self.querier.querysd(0x09, 0x01);
+
+#
+# The fanlinc device has lots of other features, none of it implemented
+# yet. This is where *you* come in...
+#
