b0ac6b0bae63b63ff634b8658b51ec287eeaaa2f
Daniel Pfrommer <dan.pfrommer@gmail.com>
2017-11-22 16:16:55 -0500

    Moved from nrjavaserial to jSerialComm

--- src/us/pfrommer/insteon/msg/serial/SerialIOStream.java
+++ src/us/pfrommer/insteon/msg/serial/SerialIOStream.java
@@ -1,11 +1,6 @@
 package us.pfrommer.insteon.msg.serial;
 
-import gnu.io.CommPort;
-import gnu.io.CommPortIdentifier;
-import gnu.io.NoSuchPortException;
-import gnu.io.PortInUseException;
-import gnu.io.SerialPort;
-import gnu.io.UnsupportedCommOperationException;
+import com.fazecast.jSerialComm.SerialPort;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -18,7 +13,6 @@ public class SerialIOStream implements IOStream {
 	private OutputStream	m_out;
 	
 	private	SerialPort		m_port		= null;
-	private	final String	m_appName	= "PLM";
 	private final int		m_speed		= 19200; // baud rate
 	private String			m_devName 	= null;
 	
@@ -34,41 +28,16 @@ public class SerialIOStream implements IOStream {
 	@Override
 	public void open() throws IOException {
 		try {
-			/* by default, RXTX searches only devices /dev/ttyS* and
-			 * /dev/ttyUSB*, and will so not find symlinks. The
-			 *  setProperty() call below helps 
-			 */
-			System.setProperty("gnu.io.rxtx.SerialPorts", m_devName);
-			CommPortIdentifier ci =
-					CommPortIdentifier.getPortIdentifier(m_devName);
-			CommPort cp = ci.open(m_appName, 1000);
-			if (cp instanceof SerialPort) {
-				m_port = (SerialPort)cp;
-			} else {
-				throw new IllegalStateException("unknown port type");
-			}
-			m_port.setSerialPortParams(m_speed, SerialPort.DATABITS_8,
-					SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
-			m_port.setFlowControlMode(SerialPort.FLOWCONTROL_NONE);
-			m_port.disableReceiveFraming();
-			m_port.disableReceiveThreshold();
-			//m_port.enableReceiveThreshold(1000);
-			//Do receive timeout so we can check if the reader has to stop
-			m_port.enableReceiveTimeout(1000);
-			
-			m_in	= m_port.getInputStream();
-			m_out	= new NoFlushOutputStream(m_port.getOutputStream());
-		} catch (IOException e) {
-			throw e;
-		} catch (PortInUseException e) {
-			throw new IOException(e);
-		} catch (UnsupportedCommOperationException e) {
-			throw new IOException(e);
-		} catch (NoSuchPortException e) {
-			throw new IOException(e);
-		} catch (IllegalStateException e) {
-			throw new IOException(e);
-		}
+            m_port = SerialPort.getCommPort(m_devName);
+            m_port.setComPortParameters(m_speed, 8, SerialPort.ONE_STOP_BIT, SerialPort.NO_PARITY);
+            m_port.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
+            m_port.setFlowControl(SerialPort.FLOW_CONTROL_DISABLED);
+            m_port.openPort();
+            m_in = m_port.getInputStream();
+            m_out = m_port.getOutputStream();
+        } catch (Exception e) {
+            throw new IOException(e);
+        }
 	}
 
 	@Override
@@ -76,7 +45,7 @@ public class SerialIOStream implements IOStream {
 		if (m_in != null) m_in.close();
 		if (m_out != null) m_out.close();
 		if (m_port != null) {
-			m_port.close();
+			m_port.closePort();
 		}
 		m_port = null;
 		m_in = null;
