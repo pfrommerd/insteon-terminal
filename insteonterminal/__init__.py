e82f0da56bede9dff08f7fa8bf3112c09d714f68
Daniel Pfrommer <dan.pfrommer@gmail.com>
2018-12-26 23:38:40 -0500

    Logging fully set up

--- insteonterminal/__init__.py
+++ insteonterminal/__init__.py
@@ -2,6 +2,51 @@ from terminal.interpreter import InterpreterSetup
 from terminal.shell import Shell,LoadScript
 from terminal.console import ConsoleTerminal,ConsoleCommands
 from terminal.commands import Commands
+from terminal.component import Component
+
+import logbook
+import builtins
+
+# These two loggers
+# will not be printed by the stream logger
+console_logger = logbook.Logger('console')
+prompt_logger = logbook.Logger('prompt')
+
+# The logging setup
+class LoggingSetup(Component):
+    def __init__(self, stdout, stderr):
+        super().__init__('logging')
+        self._stdout = stdout
+        self._stderr = stderr
+        self._original_print = builtins.print
+        self._stream_handler = logbook.StreamHandler(self._stdout, bubble=True,
+                                filter=lambda r,h: r.channel != 'console' and r.channel != 'prompt')
+
+        def format(record, handler):
+            if record.level != logbook.INFO:
+                return logbook.get_level_name(record.level) + ': ' + record.message
+            else:
+                return record.message
+        self._stream_handler.formatter = format
+
+        self._file_handler = logbook.FileHandler('insteon-terminal.log', bubble=True)
+
+    def init(self, shell):
+        def print(*args, **kwargs):
+            self._original_print(*args, **kwargs)
+            lines = ' '.join(map(str, args)).split('\n')
+            for l in lines:
+                console_logger.info(l)
+        builtins.print = print
+
+        self._file_handler.push_application()
+        self._stream_handler.push_application()
+
+    def dispose(self, shell):
+        builtins.print = self._original_print
+
+        self._stream_handler.pop_application()
+        self._file_handler.pop_application()
 
 def run():
     shell = Shell()
@@ -10,8 +55,10 @@ def run():
     shell.add_resource_path('/usr/share/insteon-terminal')
 
     terminal = ConsoleTerminal()
+    terminal.add_prompt_listener(lambda p,v: prompt_logger.info(v))
 
     shell.add_component(InterpreterSetup())
+    shell.add_component(LoggingSetup(terminal.stdout, terminal.stderr))
     shell.add_component(Commands())
     shell.add_component(ConsoleCommands(terminal))
     shell.add_component(LoadScript('sys_init.py'))
